135d74ff1461598b2348dc165fcebfe17e7bd5f4
John Barton (joho) <jrbarton@gmail.com>
2009-05-12 22:10:42 +1000

    adding a stupid atom feed

--- blog.rb
+++ blog.rb
@@ -4,6 +4,46 @@ require 'rdiscount'
 require 'hpricot'
 require 'open-uri'
 
+class Time
+  def xmlschema
+    strftime("%Y-%m-%dT%H:%M:%S%Z")
+  end unless defined?(xmlschema)
+end
+
+class Post
+  def self.all
+    file_names.collect do |e|
+      doc = Hpricot(load_html("posts/#{e}"))
+      [e.split('.')[0], 
+      doc.at('h2').inner_text] 
+    end
+  end
+  
+  def self.all_for_feed
+    file_names[0, 10].collect do |e|
+      html = load_html("posts/#{e}")
+      doc = Hpricot(html)
+      stat = File::Stat.new("posts/#{e}")
+      
+      details = { :file_name => e.split('.')[0],
+                  :title => doc.at('h2').inner_text,
+                  :published => stat.ctime,
+                  :body => RDiscount.new(html).to_html }
+      details[:updated] = stat.mtime if stat.mtime != stat.ctime
+      
+      details
+    end
+  end
+  
+  def self.file_names
+    Dir.entries('posts').select do |entry| 
+      entry =~ /\.md/
+    end.sort_by do |file_name|
+      File.ctime("posts/#{file_name}")
+    end.reverse
+  end
+end
+
 def load_html(file_path)
   file_contents = File.open(file_path, 'r') { |f| f.read }
   RDiscount.new(file_contents).to_html
@@ -14,17 +54,7 @@ get '/' do
 end
 
 get '/posts' do
-  file_names = Dir.entries('posts').select do |entry| 
-    entry =~ /\.md/
-  end.sort_by do |file_name|
-    File.ctime("posts/#{file_name}")
-  end
-  
-  @posts = file_names.collect do |e|
-    doc = Hpricot(load_html("posts/#{e}"))
-    [e.split('.')[0], 
-    doc.at('h2').inner_text] 
-  end
+  @posts = Post.all
 
   haml :posts
 end
@@ -36,6 +66,13 @@ get '/posts/:file_name' do
   @content = load_html(file_path)
   
   haml :post
+end
+
+get '/feed.atom' do
+  @posts = Post.all_for_feed
+  @updated_date = @posts.first[:updated] || @posts.first[:published]
+  content_type 'application/atom+xml'
+  haml :feed, :layout => false
 end
 
 get '/styles.css' do
