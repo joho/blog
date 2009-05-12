3d3bfa94e58b4ae98217e8b588b2de66a83edfd4
John Barton (joho) <jrbarton@gmail.com>
2009-05-12 19:50:19 +1000

    remove all the active record blergh. load all the old blog posts

--- blog.rb
+++ blog.rb
@@ -1,27 +1,11 @@
 require 'rubygems'
 require 'sinatra'
-require 'yaml'
-require 'activerecord'
-require 'digest/md5'
 require 'rdiscount'
 require 'hpricot'
-
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :dbfile =>  'blog.db'
-)
 
 def load_html(file_path)
   file_contents = File.open(file_path, 'r') { |f| f.read }
   RDiscount.new(file_contents).to_html
-end
-
-class Post < ActiveRecord::Base
-  set_table_name :blog_posts
-  
-  def summary
-    body[0, [100, body.size].min]
-  end
 end
 
 get '/' do
@@ -29,7 +13,9 @@ get '/' do
 end
 
 get '/posts' do
-  file_names = %w(test.md)
+  # need to order these by created time
+  file_names = Dir.entries('posts').select {|p| p =~ /\.md/}
+  
   @posts = file_names.collect do |e|
     doc = Hpricot(load_html("posts/#{e}"))
     [e.split('.')[0], 
